984ee325bc88dac653535023cb3d247b9fb7e27f
floursifter <88210934+floursifter@users.noreply.github.com>
2021-07-30 15:41:08 -0600

    Create ps1a.py

---
+++ Kyle/6.0001/PS1/ps1a.py
@@ -0,0 +1,31 @@
+import numpy as np
+import pandas as pd
+
+def AdvanceMonth(moneyIn, mo_salary, per_saved, interest):
+    interest_add = moneyIn + moneyIn*interest/12
+    new_savings = interest_add + mo_salary*per_saved
+
+    return new_savings
+  
+def GetInput(question: str):
+    val = input('Enter ' + question + ": ")
+    return val
+  
+# Problem A
+if __name__ == "__main__":
+    PORTION_DOWN_PAYMENT = 0.25
+    INTEREST_RATE = 0.04
+    current_savings = 0
+    
+    annual_salary = float(GetInput("your annual salary"))
+    monthly_salary = annual_salary/12
+    portion_saved = float(GetInput("the percent of your salary to save, as a decimal"))
+    total_cost = float(GetInput("the cost of your dream home"))
+    month_count = 0
+    
+    while(current_savings < total_cost*PORTION_DOWN_PAYMENT):
+        #print(current_savings)
+        current_savings = AdvanceMonth(current_savings, monthly_salary, portion_saved, INTEREST_RATE)
+        month_count+=1
+    
+    print("Number of months:", month_count)
